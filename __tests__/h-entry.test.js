6fc5ac852c614aaeced69fd37f4f109a3a517e7d
Remy Sharp <remy@remysharp.com>
2019-06-18 08:27:13 +0100

    fix: edge case where h-entry is empty

---
+++ __tests__/h-entry.test.js
@@ -0,0 +1,31 @@
+const tap = require('tap');
+const { findEntries } = require('../lib/microformat/dom');
+
+const fixture = {
+  items: [
+    {
+      type: ['h-entry'],
+      properties: {
+        name: [
+          'Indie Map is a public IndieWeb social graph and dataset.\n\n\n\n2300 sites, 5.7M pages, 380GB HTML with microformats2.\nSocial graph API and interactive map: 631M links, 706K relationships.\nSQL queryable dataset, stats, raw crawl data.\n\n\nLearn more ➜',
+        ],
+      },
+    },
+  ],
+  rels: {
+    stylesheet: ['style.css'],
+  },
+  'rel-urls': {
+    'style.css': {
+      type: 'text/css',
+      rels: ['stylesheet'],
+    },
+  },
+};
+
+tap.test('empty h-entry', t => {
+  const res = findEntries(fixture.items);
+
+  t.equal(res.length, 0);
+  t.end();
+});
