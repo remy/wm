c3917b6503ac3538691b3e8553593e575266b050
Anthony Ciccarello <aciccarello@sitepen.com>
2023-02-14 14:43:30 -0800

    fix tests
    involves updating node env for development

--- __tests__/request.test.js
+++ __tests__/request.test.js
@@ -1,15 +1,17 @@
 const tap = require('tap');
 const request = require('../lib/request');
 
-tap.test('request a duff domain', t => {
-  t.plan(1);
+tap.test('request a slow domain', t => {
+  t.plan(2);
   const timeout = 2000;
   const now = Date.now();
-  request('http://www.communitymx.com', timeout)
+  request('https://httpstat.us/200?sleep=5000', timeout)
     .then(() => {
       t.fail('should not resolve');
     })
     .catch(e => {
-      t.ok(Date.now() - now < timeout + 1000);
+      const duration = Date.now() - now;
+      t.ok(duration > timeout);
+      t.ok(duration < timeout + 1000);
     });
 });
